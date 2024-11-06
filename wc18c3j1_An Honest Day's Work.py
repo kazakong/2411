b4892efe44e73ee762eda10657b4e98ede793362
khk <kazakong@naver.com>
2024-11-06 16:47:44 +0900

    Merge remote-tracking branch '2411/master'

---
+++ wc18c3j1_An Honest Day's Work.py
@@ -0,0 +1,9 @@
+P = int(input()) #첫번째 줄, P = 1 =< P =< 100
+B = int(input()) #두번째 줄, B = 1 =< B =< 100
+D = int(input()) #세번째 줄, D = 1 =< D =< 100
+
+if P < 1 or P >  100 and B < 1 or B > 100 and D < 1 or D > 100:
+    exit()
+
+
+
