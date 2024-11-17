b590fe93af1fc08585cba3edebe84d5b553d61c6
khk <kazakong@naver.com>
2024-11-17 18:40:57 +0900

    excel

---
+++ excel_1.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# 엑셀 파일 경로를 지정
+file_path = 'C:/Users/khk/Desktop/일자리 창출 확인서.xlsx'  # 경로 수정
+
+# 엑셀 파일 읽기
+df = pd.read_excel(file_path)
+
+df = pd.read_excel(file_path, sheet_name='Sheet2')
+row_data = df.iloc[3]
+filtered_values = row_data[(row_data >= 15) & (row_data <= 34)]
+
+print(filtered_values.xlsx)
+
+
+
+
